d03a0826a546c225d125398ad86a9772a006c60f
Timothy Ward <unwrittenrainbow@Timothys-MacBook-Pro.local>
2013-11-15 23:09:51 -0800

    Initial Commit

---
+++ Peace Corps Near Me/Peace Corps Near Me/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Peace Corps Near Me
+//
+//  Created by Timothy Ward on 11/15/13.
+//  Copyright (c) 2013 i School Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Peace Corps Near Me/Peace Corps Near MeTests/Peace_Corps_Near_MeTests.m
@@ -0,0 +1,34 @@
+//
+//  Peace_Corps_Near_MeTests.m
+//  Peace Corps Near MeTests
+//
+//  Created by Timothy Ward on 11/15/13.
+//  Copyright (c) 2013 i School Hackathon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Peace_Corps_Near_MeTests : XCTestCase
+
+@end
+
+@implementation Peace_Corps_Near_MeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
